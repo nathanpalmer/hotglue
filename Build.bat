1376d49a359d5fa63b1be1e5c72f267fb00f0986
Nathan Palmer <nate@thinktheweb.com>
2012-05-22 19:05:56 -0400

    Updated build to pull nuget packages from test and core. Also integrated https://github.com/nathanpalmer/advanced-build

--- Build.bat
+++ Build.bat
@@ -1,1 +1,8 @@
-  .\Tools\Nuget.exe install .\Source\HotGlue.Tests\packages.config -OutputDirectory .\Libraries\
+@echo off
+
+.\Tools\Nuget.exe install .\Source\HotGlue\packages.config -OutputDirectory .\Libraries\
+.\Tools\Nuget.exe install .\Source\HotGlue.Tests\packages.config -OutputDirectory .\Libraries\
+  
+rmdir Build /q/s 
+mkdir Build
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe .\Source\HotGlue.sln /p:OutDir=%CD%\Build\ /p:Config=Release /v:Minimal
