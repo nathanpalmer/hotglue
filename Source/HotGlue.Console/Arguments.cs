f4de96385427c426ca084ba7b2186af6c1290681
CraigStuntz <craig_stuntz@acm.org>
2012-05-30 13:57:48 -0400

    Add command line shell and arguments parser. Add Build folder to .gitignore.

---
+++ Source/HotGlue.Console/Arguments.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HotGlue.Console
+{
+    internal class Arguments
+    {
+        public String Error { get; set; }
+        public IEnumerable<String> InFilenames { get; set; }
+        public String OutFilename { get; set; }
+
+        private enum SwitchType
+        {
+            None,
+            OutFilename,
+            Unknown
+        }
+
+        private enum ParseState
+        {
+            Start,
+            Infiles,
+            ExpectOutfile,
+            GotOutFile
+        }
+
+        public static Arguments Parse(IEnumerable<String> args)
+        {
+            if (args == null)
+            {
+                throw new ArgumentNullException("args");
+            }
+
+            var infiles = new List<string>();
+            var state = ParseState.Start; 
+            string outfile = null;
+
+            foreach (var arg in args)
+            {
+                var switchType = ToSwitchType(arg);
+                switch (state)
+                {
+                    case ParseState.Start:
+                        switch (switchType)
+                        {
+                            case SwitchType.None:
+                                infiles.Add(arg);
+                                state = ParseState.Infiles;
+                                break;
+                            case SwitchType.OutFilename:
+                            case SwitchType.Unknown:
+                                return UsageHelp();
+                            default:
+                                throw new InvalidOperationException("Unknown SwitchType: " + switchType.ToString());
+                        }
+                        break;
+                    case ParseState.Infiles:
+                        switch (switchType)
+                        {
+                            case SwitchType.None:
+                                infiles.Add(arg);
+                                break;
+                            case SwitchType.OutFilename:
+                                state = ParseState.ExpectOutfile;
+                                break;
+                            case SwitchType.Unknown:
+                                return UsageHelp();
+                            default:
+                                throw new InvalidOperationException("Unknown SwitchType: " + switchType.ToString());
+                        }
+                        break;
+                    case ParseState.ExpectOutfile:
+                        switch (switchType)
+                        {
+                            case SwitchType.None:
+                                outfile = arg;
+                                state = ParseState.GotOutFile;
+                                break;
+                            case SwitchType.OutFilename:
+                            case SwitchType.Unknown:
+                                return UsageHelp();
+                            default:
+                                throw new InvalidOperationException("Unknown SwitchType: " + switchType.ToString());
+                        }
+                        break;
+                    case ParseState.GotOutFile:
+                        // "Extra" params -- error
+                        return UsageHelp();
+                    default:
+                        throw new InvalidOperationException("Unknown state: " + state.ToString());
+                }
+            }
+
+            if (state == ParseState.Start)
+            {
+                // no params received
+                return UsageHelp();
+            }
+
+            return new Arguments
+                       {
+                           InFilenames = infiles,
+                           OutFilename = outfile
+                       };
+        }
+
+        public static Arguments UsageHelp()
+        {
+            return new Arguments { Error = Properties.Resources.UsageHelp };
+        }
+
+        private static SwitchType ToSwitchType(string argument)
+        {
+            if (string.IsNullOrEmpty(argument))
+            {
+                throw new ArgumentNullException("argument");
+            }
+
+            if (argument[0] != '-')
+            {
+                return SwitchType.None; ;
+            }
+
+            if ("-outfile".StartsWith(argument))
+            {
+                return SwitchType.OutFilename;
+            }
+
+            return SwitchType.Unknown;
+        }
+    }
+}
---
+++ Source/HotGlue.Tests/Console/ArgumentsTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Shouldly;
+using HotGlue.Console;
+
+namespace HotGlue.Tests.Console
+{
+    [TestFixture]
+    public class ArgumentsTests
+    {
+        [Test]
+        public void Parse_should_error_for_no_arguments()
+        {
+            var input = Enumerable.Empty<string>();
+
+            var actual = Arguments.Parse(input);
+
+            actual.Error.ShouldBe(HotGlue.Console.Properties.Resources.UsageHelp);
+        }
+
+        [Test]
+        public void Parse_should_handle_one_filename()
+        {
+            var infile = "foo.js";
+            var input = new [] { infile };
+
+            var actual = Arguments.Parse(input);
+
+            actual.InFilenames.ShouldNotBeEmpty();
+            actual.InFilenames.First().ShouldBe(infile);
+            actual.Error.ShouldBe(null);
+        }
+
+        [Test]
+        public void Parse_should_handle_outfile()
+        {
+            var infile = "foo.js";
+            var outfile = "bar.js";
+            var input = new[] { infile, "-o", outfile };
+
+            var actual = Arguments.Parse(input);
+
+            actual.InFilenames.ShouldNotBeEmpty();
+            actual.InFilenames.First().ShouldBe(infile);
+            actual.OutFilename.ShouldBe(outfile);
+            actual.Error.ShouldBe(null);
+        }
+
+        [Test]
+        public void Parse_should_handle_multiple_infilenames()
+        {
+            var infile = "foo.js";
+            var infile2 = "baz.js";
+            var outfile = "bar.js";
+            var input = new[] { infile, infile2, "-o", outfile };
+
+            var actual = Arguments.Parse(input);
+
+            actual.InFilenames.ShouldNotBeEmpty();
+            actual.InFilenames.ShouldContain(infile);
+            actual.InFilenames.ShouldContain(infile2);
+            actual.OutFilename.ShouldBe(outfile);
+            actual.Error.ShouldBe(null);
+        }
+
+        [Test]
+        public void Parse_should_error_on_extra_params()
+        {
+            var infile = "foo.js";
+            var infile2 = "baz.js";
+            var outfile = "bar.js";
+            var crap = "misplaced";
+            var input = new[] { infile, "-o", outfile, crap };
+
+            var actual = Arguments.Parse(input);
+
+            actual.Error.ShouldBe(HotGlue.Console.Properties.Resources.UsageHelp);
+        }
+
+        [Test]
+        public void Parse_should_error_on_unrecognized_switch()
+        {
+            var input = new[] { "-?" };
+
+            var actual = Arguments.Parse(input);
+
+            actual.Error.ShouldBe(HotGlue.Console.Properties.Resources.UsageHelp);
+        }
+    }
+}
