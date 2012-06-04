70fef61fd761df62c96ccce49ce497fd16560fc4
Nathan Palmer <nate@thinktheweb.com>
2012-06-04 19:20:39 -0400

    Moved HotGlue.Script to a separate file

--- Source/HotGlue.Web/Extensions.cs
+++ Source/HotGlue.Web/Extensions.cs
@@ -1,68 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Web;
-using System.Web.Configuration;
 using HotGlue.Model;
-using HotGlue.Web;
 
 namespace HotGlue
 {
-    public static class Script
-    {
-        private static HotGlueConfiguration _configuration;
-        private static bool _debug;
-        private static IReferenceLocator _locator;
-
-        static Script()
-        {
-            _configuration = HotGlueConfigurationSection.Load();
-            _debug = ((CompilationSection) ConfigurationManager.GetSection(@"system.web/compilation")).Debug;
-            _locator = new GraphReferenceLocator(_configuration);
-        }
-
-        public static HtmlString Reference(string name)
-        {
-            var context = HttpContext.Current;
-            var root = context.Server.MapPath("~");
-            var package = Package.Build(_configuration, root);
-
-            if (_debug)
-            {
-                name = name.Replace("/", "\\");
-
-                string file = name.StartsWith("\\")
-                                  ? name.Substring(1)
-                                  : Path.Combine(_configuration.ScriptPath.Replace("/", "\\"), name);
-
-                var relative = file.Substring(0, file.LastIndexOf("\\", StringComparison.Ordinal)) + "\\";
-                file = file.Substring(file.LastIndexOf("\\", StringComparison.Ordinal)+1);
-
-                var reference = new Reference
-                                {
-                                    Path = relative,
-                                    Name = file,
-                                    Type = Model.Reference.TypeEnum.App
-                                };
-
-                var references = _locator.Load(root, reference);
-
-                return new HtmlString(package.References(references));
-            }
-
-            var appName = name + "-glue";
-            var appDirectory = new DirectoryInfo(context.Server.MapPath("."));
-            var appFile = new FileInfo(Path.Combine(context.Server.MapPath("~") + _configuration.ScriptPath, appName));
-            var appReference = new SystemReference(appDirectory, appFile, appName) {Type = Model.Reference.TypeEnum.App};
-
-            return new HtmlString(package.References(new[] {appReference}));
-        }
-    }
-
     public static class Extensions
     {
         private static readonly Regex FileNameRegex = new Regex(@"(?<file>\S+)(?<extension>\.(.+(?=(-module|-glue|-require))|.+))");
---
+++ Source/HotGlue.Web/Script.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Configuration;
+using System.IO;
+using System.Web;
+using System.Web.Configuration;
+using HotGlue.Model;
+using HotGlue.Web;
+
+namespace HotGlue
+{
+    public static class Script
+    {
+        private static HotGlueConfiguration _configuration;
+        private static bool _debug;
+        private static IReferenceLocator _locator;
+
+        static Script()
+        {
+            _configuration = HotGlueConfigurationSection.Load();
+            _debug = ((CompilationSection) ConfigurationManager.GetSection(@"system.web/compilation")).Debug;
+            _locator = new GraphReferenceLocator(_configuration);
+        }
+
+        public static HtmlString Reference(string name)
+        {
+            var context = HttpContext.Current;
+            var root = context.Server.MapPath("~");
+            var package = Package.Build(_configuration, root);
+
+            if (_debug)
+            {
+                name = name.Replace("/", "\\");
+
+                string file = name.StartsWith("\\")
+                                  ? name.Substring(1)
+                                  : Path.Combine(_configuration.ScriptPath.Replace("/", "\\"), name);
+
+                var relative = file.Substring(0, file.LastIndexOf("\\", StringComparison.Ordinal)) + "\\";
+                file = file.Substring(file.LastIndexOf("\\", StringComparison.Ordinal)+1);
+
+                var reference = new Reference
+                                {
+                                    Path = relative,
+                                    Name = file,
+                                    Type = Model.Reference.TypeEnum.App
+                                };
+
+                var references = _locator.Load(root, reference);
+
+                return new HtmlString(package.References(references));
+            }
+
+            var appName = name + "-glue";
+            var appDirectory = new DirectoryInfo(context.Server.MapPath("."));
+            var appFile = new FileInfo(Path.Combine(context.Server.MapPath("~") + _configuration.ScriptPath, appName));
+            var appReference = new SystemReference(appDirectory, appFile, appName) {Type = Model.Reference.TypeEnum.App};
+
+            return new HtmlString(package.References(new[] {appReference}));
+        }
+    }
+}
