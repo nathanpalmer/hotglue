37f6d146bcbd34c41ff7ebf1ea8b171fed40ed83
Nathan Palmer <nate@thinktheweb.com>
2013-01-26 09:21:16 -0800

    Added back the LABjs reference that was removed somehow. Also updated the ASP.NET with the correct references and config

---
+++ Source/HotGlue.Reference.LABjs/LABjsScriptReference.cs
@@ -0,0 +1,18 @@
+using System.IO;
+using HotGlue.Model;
+
+namespace HotGlue
+{
+    public class LABjsScriptReference : IGenerateScriptReference
+    {
+        public string GenerateReference(SystemReference reference)
+        {
+            var relativePath = reference.RelativePath(true);
+            var wait = reference.Wait ? ".wait()" : "";
+
+            return reference.Name.EndsWith("-module")
+                       ? string.Format(".script(\"{0}&name={2}\"){1}", relativePath, wait, string.Join("&name=", reference.ReferenceNames))
+                       : string.Format(".script(\"{0}\"){1}", relativePath, wait);
+        }
+    }
+}
