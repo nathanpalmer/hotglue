93a50f10094d4498a64fbc7e8493c8f0b958388c
Nathan Palmer <nate@thinktheweb.com>
2013-01-27 08:26:48 -0800

    Added handlebars.js template support

---
+++ Source/HotGlue.Template.Handlebars/HandlebarsTemplateCompiler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using HotGlue.Model;
+
+namespace HotGlue.Compilers
+{
+    public class HandlebarsTemplateCompiler : ICompile
+    {
+        public List<string> Extensions { get; private set; }
+
+        public HandlebarsTemplateCompiler()
+        {
+            Extensions = new List<string>(new[] { ".tmpl" });
+        }
+
+        public bool Handles(string Extension)
+        {
+            return Extensions.Any(e => e == Extension);
+        }
+
+        public void Compile<T>(ref T reference) where T : Reference
+        {
+            var name = reference.Name;
+            reference.Extension = ".js";
+            reference.Content = @"
+var template = """ + reference.Content.Replace("\r\n", "").Replace("\n", "").Replace("\"", "'") + @""";
+var compiled = Handlebars.compile(template);
+module.exports = (function(data){ return compiled(data); });";
+        }
+    }
+}
