nathanpalmer/hotglue

commit da2553b7d33caa989e895a9ebfd7be1204f43f99

Allow passing multiple files to Script.Reference

Source/HotGlue.Core/Common/ScriptHelper.cs
@@ -13,35 +13,43 @@ namespace HotGlue
             LoadedConfiguration configuration, 
             IReferenceLocator locator,
             string root,
-            string name,
+            IEnumerable<string> names,
             bool debug)
         {
             var package = Package.Build(configuration, root);
+            var references = new List<SystemReference>();
 
             if (debug)
             {
-                name = name.Reslash();
+                foreach (var name in names)
+                {
+                    var cleanedName = name.Reslash();
 
-                string file = name.StartsWith("/")
-                                  ? name.Substring(1)
-                                  : Path.Combine(configuration.ScriptPath.Reslash(), name).Reslash();
-                file = file.StartsWith("/") ? file.Substring(1) : file;
+                    string file = cleanedName.StartsWith("/")
+                                      ? name.Substring(1)
+                                      : Path.Combine(configuration.ScriptPath.Reslash(), cleanedName).Reslash();
+                    file = file.StartsWith("/") ? file.Substring(1) : file;
 
-                name = file.Substring(file.LastIndexOf("/", StringComparison.Ordinal) + 1);
+                    cleanedName = file.Substring(file.LastIndexOf("/", StringComparison.Ordinal) + 1);
 
-                var reference = new SystemReference(new DirectoryInfo(root), new FileInfo(Path.Combine(root, file)), name);
+                    var reference = new SystemReference(new DirectoryInfo(root), new FileInfo(Path.Combine(root, file)), cleanedName);
 
-                var references = locator.Load(root, reference);
+                    references.AddRange(locator.Load(root, reference));
+                }
 
                 return package.GenerateReferences(references);
             }
 
-            var appName = name + "-glue";
-            var appDirectory = new DirectoryInfo(root);
-            var appFile = new FileInfo(Path.Combine(root + configuration.ScriptPath, appName));
-            var appReference = new SystemReference(appDirectory, appFile, appName) { Type = Model.Reference.TypeEnum.App };
+            foreach (var name in names)
+            {
+                var appName = name + "-glue";
+                var appDirectory = new DirectoryInfo(root);
+                var appFile = new FileInfo(Path.Combine(root + configuration.ScriptPath, appName));
+                var appReference = new SystemReference(appDirectory, appFile, appName) { Type = Model.Reference.TypeEnum.App };
+                references.Add(appReference);                
+            }
 
-            return package.GenerateReferences(new[] { appReference });
+            return package.GenerateReferences(references);
         }
 
         public static void RewriteContent(

Source/HotGlue.Aspnet/Script.cs
@@ -11,9 +11,9 @@ namespace HotGlue
 {
     public static class Script
     {
-        private static LoadedConfiguration _configuration;
-        private static bool _debug;
-        private static IReferenceLocator _locator;
+        private static readonly LoadedConfiguration _configuration;
+        private static readonly bool _debug;
+        private static readonly IReferenceLocator _locator;
 
         static Script()
         {
@@ -24,11 +24,11 @@ namespace HotGlue
             _locator = new GraphReferenceLocator(_configuration);
         }
 
-        public static IHtmlString Reference(string name)
+        public static IHtmlString Reference(params string[] names)
         {
             var context = HttpContext.Current;
             var root = context.Server.MapPath("~");
-            return new HtmlString(ScriptHelper.Reference(_configuration, _locator, root, name, _debug));
+            return new HtmlString(ScriptHelper.Reference(_configuration, _locator, root, names, _debug));
         }
     }
 }

Source/HotGlue.Nancy.Razor/Script.cs
@@ -7,9 +7,9 @@ namespace HotGlue
 {
     public static class Script
     {
-        private static LoadedConfiguration _configuration;
-        private static bool _debug;
-        private static IReferenceLocator _locator;
+        private static readonly LoadedConfiguration _configuration;
+        private static readonly bool _debug;
+        private static readonly IReferenceLocator _locator;
 
         static Script()
         {
@@ -19,10 +19,10 @@ namespace HotGlue
             _locator = new GraphReferenceLocator(_configuration);
         }
 
-        public static IHtmlString Reference(string name)
+        public static IHtmlString Reference(params string[] names)
         {
             var root = HotGlueNancyStartup.Root;
-            return new NonEncodedHtmlString(ScriptHelper.Reference(_configuration, _locator, root, name, _debug));
+            return new NonEncodedHtmlString(ScriptHelper.Reference(_configuration, _locator, root, names, _debug));
         }
     }
 }

Source/HotGlue.Nancy/Script.cs
@@ -12,9 +12,9 @@ namespace HotGlue
 {
     public static class Script
     {
-        private static LoadedConfiguration _configuration;
-        private static bool _debug;
-        private static IReferenceLocator _locator;
+        private static readonly LoadedConfiguration _configuration;
+        private static readonly bool _debug;
+        private static readonly IReferenceLocator _locator;
 
         static Script()
         {
@@ -24,10 +24,10 @@ namespace HotGlue
             _locator = new GraphReferenceLocator(_configuration);
         }
 
-        public static string Reference(string name)
+        public static string Reference(params string[] names)
         {
             var root = HotGlueNancyStartup.Root;
-            return ScriptHelper.Reference(_configuration, _locator, root, name, _debug);
+            return ScriptHelper.Reference(_configuration, _locator, root, names, _debug);
         }
     }
 }

commit 682357a3c90aabfb5ce8a3841eed56a377316520

Correct a bug introduced in ScriptHelper.cs
Commit da2553b7d33caa989e895a9ebfd7be1204f43f99 introduced a bug where
the non-Reslashed name is used in some cases. Not sure if it's actually
a problem, but different than before. Fix that.

Source/HotGlue.Core/Common/ScriptHelper.cs
@@ -25,7 +25,7 @@ namespace HotGlue
                     var cleanedName = name.Reslash();
 
                     string file = cleanedName.StartsWith("/")
-                                      ? name.Substring(1)
+                                      ? cleanedName.Substring(1)
                                       : Path.Combine(context.Configuration.ScriptPath.Reslash(), cleanedName).Reslash();
                     file = file.StartsWith("/") ? file.Substring(1) : file;
 