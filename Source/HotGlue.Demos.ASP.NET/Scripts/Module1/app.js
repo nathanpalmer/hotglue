c8f0083675a4b6720abce5776285a1b83c969d40
Nathan Palmer <nate@thinktheweb.com>
2013-02-26 18:59:44 -0800

    Added spine coffee files for testing the compiler

--- Source/HotGlue.Demos.ASP.NET/Scripts/Module1/app.js
+++ Source/HotGlue.Demos.ASP.NET/Scripts/Module1/app.js
@@ -1,5 +1,11 @@
 ﻿//= require dep1.js
 //= require ../Shared/jquery-1.7.2.js
+//= require ../spinejs/spine.coffee
+//= require ../spinejs/ajax.coffee
+//= require ../spinejs/list.coffee
+//= require ../spinejs/local.coffee
+//= require ../spinejs/manager.coffee
+//= require ../spinejs/relation.coffee
 
 $(document).ready(function() {
     $("#container").text("Loaded");
